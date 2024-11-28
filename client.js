c3f5b762e1fd05765a4bb5dc2a316342190bbbb6
normyp <pm.normington@gmail.com>
2024-11-28 21:57:58 +0000

    Show messages as they come in

--- client.js
+++ client.js
@@ -5,33 +5,16 @@ const client = dgram.createSocket({
 });
 const UDP_PORT = 33333;
 
-// Track messages and their timeouts
-const messageTimeouts = new Map();
-
 // Enable broadcast and bind to the broadcast port
 client.bind(UDP_PORT, () => {
     client.setBroadcast(true);
 });
 
-// Clear console and redraw messages
-function updateDisplay() {
-    console.clear();
-    console.log(msg);
-}
-
 // Listen for messages
-client.on('message', (msg, rinfo) => {
+client.on('message', (msg) => {
     const message = msg.toString().trim();
-    
-    // Only process new messages
-    if (message && !messageTimeouts.has(message)) {
-        // Add message with timeout
-        messageTimeouts.set(message, setTimeout(() => {
-            messageTimeouts.delete(message);
-            updateDisplay();
-        }, 5000)); // 5 seconds
-        
-        updateDisplay();
+    if (message) {
+        console.log(message);
     }
 });
 
@@ -42,5 +25,3 @@ process.stdin.on('data', (data) => {
         client.send(message, UDP_PORT, '255.255.255.255');
     }
 });
-
-console.log('Type a message and press Enter to send.');
